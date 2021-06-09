2f1dddb40523508d97219fb8e3d27e5c28fc0285
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2021-06-09 08:08:31 +0100

    reversed all

--- packages/www/functions/graphql/graphql.js
+++ packages/www/functions/graphql/graphql.js
@@ -50,7 +50,7 @@ const resolvers = {
         q.Create(q.Collection("tech-events"), {
           data: {
             title,
-            date: q.Date(date),
+            date,
             url,
             type
           }
